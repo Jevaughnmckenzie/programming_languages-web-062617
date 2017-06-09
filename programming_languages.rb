3cc840ef4493c6d17e2ee3264f88ebe3d67e44a3
Jevaughn McKenzie <jmckenzie1004@gmail.com>
2017-06-09 01:53:05 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,25 @@
 def reformat_languages(languages)
   # your code here
+
+  new_hash = {}
+
+  languages.each do |style, language_hash|
+
+    language_hash.each do |language, type_hash|
+
+
+
+      if new_hash[language]
+        new_hash[language][:style] << style
+      else
+        new_hash[language] = type_hash
+        new_hash[language][:style] = [style]
+      end
+
+    end
+
+  end
+
+  # binding.pry
+  new_hash
 end
